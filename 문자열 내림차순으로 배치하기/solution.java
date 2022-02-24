65448d6cd084765048229ffbbcfbac0b0564a489
Eulga <63146118+Eulga@users.noreply.github.com>
2022-02-24 18:39:45 +0900

    Update solution.java

--- 문자열 내림차순으로 배치하기/solution.java
+++ 문자열 내림차순으로 배치하기/solution.java
@@ -3,5 +3,15 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         return String.join("", Arrays.stream(s.split("")).sorted(Comparator.reverseOrder()).toArray(String[]::new));
+        
+        // return Arrays.stream(s.split("")).sorted(Comparator.reverseOrder()).collect(Collectors.joining());
+        // 속도 비슷한데 Collectors 를 사용함
     }
 }
+
+// char[] sol = str.toCharArray();
+// Arrays.sort(sol);
+// return new StringBuilder(new String(sol)).reverse().toString();
+// 빠름
+
+
