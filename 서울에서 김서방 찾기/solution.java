3e85b4f5cbb792e8d8d66e31862132df008dd5fe
Eulga <63146118+Eulga@users.noreply.github.com>
2022-02-24 19:50:09 +0900

    Update solution.java

--- 서울에서 김서방 찾기/solution.java
+++ 서울에서 김서방 찾기/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "김서방은 ";
-        for(int i = 0; i < seoul.length; i++) {
-            if(seoul[i].equals("Kim")) {answer += i+"에 있다"; break;}
-        }
-        return answer;
+//         String answer = "김서방은 ";
+//         for(int i = 0; i < seoul.length; i++) {
+//             if(seoul[i].equals("Kim")) {answer += i+"에 있다"; break;}
+//         }
+//         return answer;
+        
+           return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
     }
 }
