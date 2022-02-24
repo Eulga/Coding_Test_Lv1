f0cb5328ac15eaf7f43cec182eb66398bd252614
Eulga <63146118+Eulga@users.noreply.github.com>
2022-02-24 18:56:19 +0900

    Update solution.java

--- 문자열 다루기 기본/solution.java
+++ 문자열 다루기 기본/solution.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6) return false;
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch(NumberFormatException e) {
-            return false;
-        }
+//         if(s.length() != 4 && s.length() != 6) return false;
+//         try {
+//             Integer.parseInt(s);
+//             return true;
+//         } catch(NumberFormatException e) {
+//             return false;
+//         }
+        // 예외처리로 장난치면 혼난다고 한다.
+        
+           return s.length() == 4 || s.length() == 6 ? s.matches("(^[0-9]*$)"):false;
     }
+    
+    
 }
