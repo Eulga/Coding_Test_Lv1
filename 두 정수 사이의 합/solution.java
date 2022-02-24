f48f5c732f04f88214e1d593c4e6efa9059eaa7f
Eulga <63146118+Eulga@users.noreply.github.com>
2022-02-24 17:26:22 +0900

    Update solution.java

--- 두 정수 사이의 합/solution.java
+++ 두 정수 사이의 합/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public long solution(int a, int b) {
         if(a==b) return a;
-        long answer = 0;
-        for(int i = Math.min(a,b); i <= Math.max(a, b); i++) {
-            answer += (long) i;
-        }
-        return answer;
+//         long answer = 0;
+//         for(int i = Math.min(a,b); i <= Math.max(a, b); i++) {
+//             answer += (long) i;
+//         }
+//         return answer;
+        
+        return (Math.max(a,b) - Math.min(a,b) + 1) * (long)(a + b) / 2;
     }
 }
